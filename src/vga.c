de68b531cf8f17bf4080d1a4ade51b55a6f0c604
Illuminum666 <arthur1chan@gmail.com>
2023-03-31 19:08:52 -0400

    Fix compile error when exporting to cpulator

--- src/vga.c
+++ src/vga.c
@@ -4,6 +4,28 @@
 
 volatile int pixel_buffer_start;
 
+/* wait for vsync, then swap front and back buffers */
+static void wait_for_vsync(void) {
+	volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
+	*pixel_ctrl_ptr = 1; // write 1 to buffer
+	while (*(pixel_ctrl_ptr + 3) & 1); // wait for S to become 0
+}
+
+/* draw line_colored pixel at (x, y) */
+void plot_pixel(int x, int y, uint16_t line_color)
+{
+    *(uint16_t *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
+}
+
+/* reset screen to all black */
+void clear_screen(void) {
+	for (int x = 0; x < SCREEN_W; ++x) {
+		for (int y = 0; y < SCREEN_H; ++y) {
+			plot_pixel(x, y, 0x0);
+		}
+	}
+}
+
 void configure_vga(void) {
 	volatile int* pixel_ctrl_ptr = (int*)PIXEL_BUF_CTRL_BASE;
 
@@ -19,27 +41,4 @@ void configure_vga(void) {
     *(pixel_ctrl_ptr + 1) = 0xC0000000;
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
     clear_screen(); // pixel_buffer_start points to the pixel buffer
-}
-
-/* wait for vsync, then swap front and back buffers */
-static void wait_for_vsync(void) {
-	volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
-	*pixel_ctrl_ptr = 1; // write 1 to buffer
-	while (*(pixel_ctrl_ptr + 3) & 1); // wait for S to become 0
-}
-
-/* draw line_colored pixel at (x, y) */
-void plot_pixel(int x, int y, uint16_t line_color)
-{
-    *(uint16_t *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
-}
-
-/* reset screen to all black */
-void clear_screen(void)
-{
-	for (int x = 0; x < SCREEN_W; ++x) {
-		for (int y = 0; y < SCREEN_H; ++y) {
-			plot_pixel(x, y, 0x0);
-		}
-	}
 }
