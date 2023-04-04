0e622a9c704a03351c01fc7c14a41a7adeee4cf8
Illuminum666 <arthur1chan@gmail.com>
2023-04-03 20:13:11 -0400

    Add 5 more cards

--- include/assets.h
+++ include/assets.h
@@ -3,6 +3,18 @@
 
 #include "image_data.h"
 
+// blade_storm.png
+extern struct image blade_storm;
+extern uint16_t blade_storm_data[];
+
+// emerald_towers.png
+extern struct image emerald_towers;
+extern uint16_t emerald_towers_data[];
+
+// execution.png
+extern struct image execution;
+extern uint16_t execution_data[];
+
 // felflares.png
 extern struct image felflares;
 extern uint16_t felflares_data[];
@@ -21,5 +33,13 @@ extern char navy_seals_data[];
 // stormbound.png
 extern struct image stormbound;
 extern uint16_t stormbound_data[];
+
+// summon_militia.png
+extern struct image summon_militia;
+extern uint16_t summon_militia_data[];
+
+// victors_of_the_melee.png
+extern struct image victors_of_the_melee;
+extern uint16_t victors_of_the_melee_data[];
 
 #endif
