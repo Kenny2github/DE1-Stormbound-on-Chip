6898223d5a5fdd6428c86ef1f9093a5d411c8077
Illuminum666 <arthur1chan@gmail.com>
2023-04-06 16:59:55 -0400

    Add deck building logic

--- src/game.c
+++ src/game.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <string.h>
 #include "assets.h"
 #include "image_data.h"
@@ -7,6 +8,10 @@
 #include "vga.h"
 
 int game_state, turn_state, move_state, player_state;
+
+bool in_deck[30];
+int deck[2][10];
+int card_num;
 
 struct card cards[30] = {
 	{"Lawless Herd", NEUTRAL, &lawless_herd},
@@ -76,6 +81,8 @@ void run_game() {
 			if (mouse_state.left_clicked) {
 				game_state = DECK;
 				player_state = P1;
+				card_num = 0;
+				for (int i = 0; i < 30; ++i) in_deck[i] = false;
 			}
 			break;
 		case DECK:
@@ -85,8 +92,59 @@ void run_game() {
 					clear_char_screen();
 					write_string(1, 1, "Cards:");
 					for (int i = 0; i < 30; ++i) {
+						if (in_deck[i]) continue;
 						draw_img_map((int)(i / 10) * 80, i % 10 * 12 + 12, *card_selection_box[cards[i].type]);
 						write_string((int)(i / 10) * 20 + (20 - strlen(cards[i].name)) / 2, i % 10 * 3 + 4, cards[i].name);
+					}
+					write_string(61, 1, "Your deck:");
+					for (int i = 0; i < card_num; ++i) {
+						draw_img_map(240, i * 12 + 12, *card_selection_box[cards[deck[player_state][i]].type]);
+						write_string(60 + (20 - strlen(cards[deck[player_state][i]].name)) / 2, i * 3 + 4, cards[deck[player_state][i]].name);
+					}
+					// draw mouse
+					draw_rectangle(mouse_state.x, mouse_state.y, 2, 2, WHITE);
+
+
+					if (mouse_state.left_clicked) {
+						if (card_num != 10
+						&& mouse_state.x >= 0 && mouse_state.x < 240
+						&& mouse_state.y >= 12 && mouse_state.y < 132) {
+
+							int idx = (int)(mouse_state.x / 80) * 10 + (int)((mouse_state.y - 12) / 12);
+							if (!(in_deck[idx])) {
+								in_deck[idx] = true;
+								deck[player_state][card_num++] = idx;
+							}
+
+						} else if (mouse_state.x >= 240 && mouse_state.x < SCREEN_W
+						&& mouse_state.y >= 12 && mouse_state.y < (card_num * 12 + 12)) {
+
+							int idx = (mouse_state.y - 12) / 12;
+							in_deck[deck[player_state][idx]] = false;
+							for (int i = idx; i < card_num - 1; ++i) {
+								deck[player_state][i] = deck[player_state][i + 1];
+							}
+							--card_num;
+							
+						}
+					} else {
+						if (card_num != 10
+						&& mouse_state.x >= 0 && mouse_state.x < 240
+						&& mouse_state.y >= 12 && mouse_state.y < 132) {
+
+							int idx = (int)(mouse_state.x / 80) * 10 + (int)((mouse_state.y - 12) / 12);
+							if (!(in_deck[idx])) {
+								draw_img_map(40, 156, *cards[idx].img);
+							}
+							
+
+						} else if (mouse_state.x >= 240 && mouse_state.x < SCREEN_W
+						&& mouse_state.y >= 12 && mouse_state.y < (card_num * 12 + 12)) {
+
+							int idx = (mouse_state.y - 12) / 12;
+							draw_img_map(20, 156, *cards[deck[player_state][idx]].img);
+							
+						}
 					}
 					
 			}
