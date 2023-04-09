36acb756ab808c7f84d5856180f9da16d24f7264
Illuminum666 <arthur1chan@gmail.com>
2023-04-09 15:33:15 -0400

    Implement deckbuilding rendering

--- src/deckbuilding.c
+++ src/deckbuilding.c
@@ -11,6 +11,25 @@
 int card_num;
 bool in_deck[30];
 
+static void display_card_info(int card_id) {
+	if (card_id != deckbuilding_cur_display) {
+		push_image(20, 156, card_data[card_id].img);
+		write_string(28, 43, empty_desc_data);
+		write_string(28, 43, card_data[card_id].desc);
+		deckbuilding_cur_display = card_id;
+	} else if (!deckbuilding_displaying) {
+		push_image(20, 156, card_data[deckbuilding_cur_display].img);
+		write_string(28, 43, card_data[deckbuilding_cur_display].desc);
+	}
+	deckbuilding_displaying = true;
+}
+
+static void clear_card_info() {
+	deckbuilding_displaying = false;
+	push_image(20, 156, &clear_card);
+	write_string(28, 43, empty_desc_data);
+}
+
 void init_deckbuilding() {
 	fill_screen(BACKGROUND);
 	clear_char_screen();
@@ -22,6 +41,7 @@ void init_deckbuilding() {
 	}
 	write_string(61, 1, player_state == P1 ? "P1 deck:" : "P2 deck:");
 	deckbuilding_displaying = false;
+	deckbuilding_cur_display = 100;
 	deckbuilding_done_displaying = false;
 	card_num = 0;
 	for (int i = 0; i < 30; ++i) in_deck[i] = false;
@@ -49,11 +69,15 @@ void run_deckbuilding(void) {
 					in_deck[idx] = true;
 					deck[player_state][card_num] = idx;
 					deckbuilding_deck_surfs[card_num] = (struct surface){240, card_num * 12 + 12, card_selection_box[card_data[deck[player_state][card_num]].faction]};
+					// draw card on deck
 					r_stack_push(deckbuilding_deck_surfs[card_num]);
-					write_string(60, card_num * 3 + 4, card_data[deck[player_state][card_num]].desc);
-					deckbuilding_card_surfs[idx] = (struct surface){(int)(idx / 10) * 80, (idx % 10) * 12 + 12, &clear_box};
-					r_stack_push(deckbuilding_card_surfs[idx]);
+					// write card name on deck
+					write_string(60 + (20 - strlen(card_data[deck[player_state][card_num]].name)) / 2, card_num * 3 + 4, card_data[deck[player_state][card_num]].name);
+					// clear card in collection
+					push_image((int)(idx / 10) * 80, (idx % 10) * 12 + 12, &clear_box);
+					// clear name
 					write_string((int)(idx / 10) * 20, (idx % 10) * 3 + 4, empty_card_name_data);
+					// if deck is full, display done button
 					if (++card_num == 10) {
 						push_image(SCREEN_W - 61, 156, &cardbuilding_done);
 						deckbuilding_done_displaying = true;
@@ -64,13 +88,26 @@ void run_deckbuilding(void) {
 			 && saved_mouse_states[0].y >= 12 && saved_mouse_states[0].y < (card_num * 12 + 12)) {	// clicked on deck
 
 				int idx = (saved_mouse_states[0].y - 12) / 12;
-				in_deck[deck[player_state][idx]] = false; // remove card from deck
+				int card_id = deck[player_state][idx];
+				in_deck[card_id] = false; // remove card from deck
 				for (int i = idx; i < card_num - 1; ++i) {	// shift deck cards down
 					deck[player_state][i] = deck[player_state][i + 1];
-					deckbuilding_deck_surfs[i] = deckbuilding_deck_surfs[i + 1];
+					deckbuilding_deck_surfs[i].data = deckbuilding_deck_surfs[i + 1].data;
+					// update card box
 					r_stack_push(deckbuilding_deck_surfs[i]);
+					// clear name
+					write_string(60, i * 3 + 4, empty_card_name_data);
+					// rewrite new card name
+					write_string(60 + (20 - strlen(card_data[deck[player_state][i]].name)) / 2, i * 3 + 4, card_data[deck[player_state][i]].name);
 				}
-				deckbuilding_deck_surfs[card_num] = (struct surface){240, card_num * 12 + 12, &clear_box};
+				// clear last item box
+				push_image(240, card_num * 12, &clear_box);
+				// clear last item name
+				write_string(60, card_num * 3 + 1, empty_card_name_data);
+				// add back item box to collection
+				push_image((int)(card_id / 10) * 80, (card_id % 10) * 12 + 12, card_selection_box[card_data[card_id].faction]);
+				// add back item name to collection
+				write_string((int)(card_id / 10) * 20 + (20 - strlen(card_data[card_id].name)) / 2, (card_id % 10) * 3 + 4, card_data[card_id].name);
 				if (card_num-- == 10) {
 					push_image(SCREEN_W - 61, 156, &clear_card);
 					deckbuilding_done_displaying = false;
@@ -82,6 +119,7 @@ void run_deckbuilding(void) {
 					case P1: // go to next player
 						player_state = P2;
 						init_deckbuilding();
+						break;
 					case P2: // go to turn state
 						init_turn();
 				}
@@ -92,30 +130,22 @@ void run_deckbuilding(void) {
 				&& saved_mouse_states[0].y >= 12 && saved_mouse_states[0].y < 132) {
 
 				int idx = (int)(saved_mouse_states[0].x / 80) * 10 + (int)((saved_mouse_states[0].y - 12) / 12);
-				if (!(in_deck[idx]) && idx != deckbuilding_cur_display) {
-					deckbuilding_displaying = true;
-					push_image(20, 156, card_data[idx].img);
-					write_string(28, 43, empty_desc_data);
-					write_string(28, 43, card_data[idx].desc);
+				if (!in_deck[idx]) {
+					display_card_info(idx);
+				} else {
+					clear_card_info();
 				}
 
 
 			} else if (saved_mouse_states[0].x >= 240 && saved_mouse_states[0].x < SCREEN_W
 				&& saved_mouse_states[0].y >= 12 && saved_mouse_states[0].y < (card_num * 12 + 12)) {
 
-				int idx = (saved_mouse_states[0].y - 12) / 12;
-				if (!(in_deck[idx]) && deck[player_state][idx] != deckbuilding_cur_display) {
-					deckbuilding_displaying = true;
-					push_image(20, 156, card_data[deck[player_state][idx]].img);
-					write_string(28, 43, empty_desc_data);
-					write_string(28, 43, card_data[deck[player_state][idx]].desc);
-				}
+				int card_id = deck[player_state][(int)((saved_mouse_states[0].y - 12) / 12)];
+				display_card_info(card_id);
 
 			} else {
 				if (deckbuilding_displaying) {
-					deckbuilding_displaying = false;
-					push_image(20, 156, &clear_card);
-					write_string(28, 43, empty_desc_data);
+					clear_card_info();
 				}
 			}
 		}
@@ -133,6 +163,7 @@ void run_deckbuilding(void) {
 		}
 		// re-render affected surfaces
 		for (int i = 0; i < NUM_CARDS; ++i) {
+			if (in_deck[i]) continue;
 			for (int j = 1; j < NUM_MOUSE_STATES; ++j) {
 				if (rects_collide(
 					// this surf's rect
@@ -144,7 +175,7 @@ void run_deckbuilding(void) {
 				)) r_stack_push(deckbuilding_card_surfs[i]);
 			}
 		}
-		for (int i = 0; i < 10; ++i) {
+		for (int i = 0; i < card_num; ++i) {
 			for (int j = 1; j < NUM_MOUSE_STATES; ++j) {
 				if (rects_collide(
 					// this surf's rect
