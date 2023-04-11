6296eb3a7f157433f2765fffa45bdc899455b943
Illuminum666 <arthur1chan@gmail.com>
2023-04-11 16:32:42 -0400

    Remove key button tests

--- src/main.c
+++ src/main.c
@@ -8,33 +8,10 @@
 #include "vga.h"
 #include "game.h"
 
-int seg7[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67, 0x063f};
-
-static void config_KEYs(void) {
-	volatile int* KEY_ptr = (int*)KEY_BASE;
-	*(KEY_ptr + 2) = 0xF; // enable interrupts for the two KEYs
-}
-
-static void pushbutton_ISR(void) {
-	volatile int* KEY_ptr = (int*)KEY_BASE;
-	volatile int* HEX30_ptr = (int*)HEX3_HEX0_BASE;
-	int press, HEX_bits;
-	press = *(KEY_ptr + 3); // read the pushbutton interrupt register
-	*(KEY_ptr + 3) = press; // Clear the interrupt
-
-	if (press & 0x1) HEX_bits = 0b00111111; // KEY0
-	else if (press & 0x2) HEX_bits = 0b00000110; // KEY1
-	else if (press & 0x4) HEX_bits = 0b01011011; // KEY2
-	else HEX_bits = 0b01001111; // KEY3
-
-	*HEX30_ptr = HEX_bits;
-}
-
 struct mouse_state_t saved_mouse_states[NUM_MOUSE_STATES];
 
 int main(void) {
 	configure_vga();
-	config_interrupt(IRQ_KEY, &config_KEYs, &pushbutton_ISR);
 	enable_mouse();
 	enable_timer();
 	enable_intval_timer();
