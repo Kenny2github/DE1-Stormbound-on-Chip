357698c869e7f55bdf181e923bddac11e6752790
AbyxDev <ken@abyx.dev>
2023-04-11 16:30:37 -0400

    Update Dark Harvest description C file

--- src/assets/card_descs/dark_harvest_desc.txt.c
+++ src/assets/card_descs/dark_harvest_desc.txt.c
@@ -7,7 +7,6 @@ const char dark_harvest_desc_data[] =
 	"Dark Harvest\n"
 	"------------------------\n"
 	"Deal 2 damage to all\n"
-	"enemy units and\n"
-	"structures surrounding\n"
-	"any friendly unit."
+	"enemies surrounding any\n"
+	"friendly unit."
 ;
